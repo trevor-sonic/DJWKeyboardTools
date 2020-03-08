a2cfd1ff0f8d27cb5d7157a0e35226e4f73bcf1e
Trevor <trevor.d@beydag.com>
2020-03-08 15:00:48 +0000

    SPM config correction.

--- Package.swift
+++ Package.swift
@@ -14,18 +14,16 @@ let package = Package(
     
     
     products: [
-        // Products define the executables and libraries produced by a package, and make them visible to other packages.
         .library(
             name: "DJWKeyboardTools",
             targets: ["DJWKeyboardTools"]),
     ],
+    
     dependencies: [
-        .package(path: "../DJWLibrary/DJWBaseVC/"),
+        .package(path: "../DJWBaseVC/"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages which this package depends on.
-        .target(
+       .target(
             name: "DJWKeyboardTools",
             dependencies: ["DJWBaseVC"]),
         .testTarget(
