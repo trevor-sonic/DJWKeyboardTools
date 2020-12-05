fe52e166d5fde7bd7397900cc08710ae5423086d
Trevor <trevor.d@beydag.com>
2020-12-05 15:13:36 +0000

    Auto update.

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
     ],
     
     dependencies: [
-        .package(url: "https://gitlab.com/trevorsonic/DJWBaseVC.git", from:"1.0.0"),
+        .package(url: "https://github.com/trevor-sonic/DJWBaseVC.git", from:"1.0.0"),
         
         ///public
         .package(url: "https://github.com/SnapKit/SnapKit.git", from: "5.0.0")
